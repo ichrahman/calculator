2368088e23f475321ed14e59111df9d077d2ea0f
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-03-06 23:55:24 +0100

    Clear display

--- script.js
+++ script.js
@@ -7,7 +7,57 @@ const equalsButton = document.querySelector('.equalsButton');
 const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[data-operator]')
 
+let firstNumber;
+let secondNumber;
+let operator;
+let flag = false;
+
+// clear display
+clear.addEventListener('click', () => {
+    display.textContent = "";
+})
+
 
 numberButtons.forEach(button => button.addEventListener('click', () => {
+    if (flag) {
+        display.textContent = '';
+        flag = !flag;
+    }
     display.textContent += button.textContent;
+
 }))
+
+function operate(firstNumber, secondNumber, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case 'x':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            result = "Error: Invalid operator";
+    }
+
+    return result;
+}
+
+
+operatorButtons.forEach(button => button.addEventListener('click', () => {
+    flag = !flag;
+    operator = button.textContent;
+    firstNumber = parseFloat(display.textContent);
+}))
+
+equalsButton.addEventListener('click', () => {
+    secondNumber = parseFloat(display.textContent);
+    let result = operate(firstNumber, secondNumber, operator);
+    display.textContent = result;
+})
